b91e5d273abc89e04afb379086c19952b76ffa1e
xuefei <13273726905@163.com>
2023-08-28 23:31:21 +0800

    spring源码阅读 - 4

--- man-spring/show/src/main/java/com/itheima/a06/A06.java
+++ man-spring/show/src/main/java/com/itheima/a06/A06.java
@@ -29,25 +29,20 @@ public class A06 {
         context.registerBean(AutowiredAnnotationBeanPostProcessor.class);
         context.registerBean(CommonAnnotationBeanPostProcessor.class);
         context.registerBean(ConfigurationClassPostProcessor.class);
-
         /*
             2. 有同学说: b、c、d 的功能用 @Autowired 就能实现啊, 为啥还要用 Aware 接口呢
             简单地说:
                 a. @Autowired 的解析需要用到 bean 后处理器, 属于扩展功能
                 b. 而 Aware 接口属于内置功能, 不加任何扩展, Spring 就能识别
             某些情况下, 扩展功能会失效, 而内置功能不会失效
-
             例1: 你会发现用 Aware 注入 ApplicationContext 成功, 而 @Autowired 注入 ApplicationContext 失败
          */
-
         /*
             例2: Java 配置类在添加了 bean 工厂后处理器后,
                 你会发现用传统接口方式的注入和初始化仍然成功, 而 @Autowired 和 @PostConstruct 的注入和初始化失败
          */
-
         context.refresh(); // 1. beanFactory 后处理器,  2. 添加 bean 后处理器, 3. 初始化单例
         context.close();
-
         /*
             学到了什么
                 a. Aware 接口提供了一种【内置】 的注入手段, 可以注入 BeanFactory, ApplicationContext
